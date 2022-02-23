0c73fe7c508cb8ad4f3f209ca815316caf3625ce
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-02-23 15:30:43 -0300

    with working reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,9 +9,28 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
+
+      # Only allow a list of trusted parameters through.
+      def restaurant_params
+        params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+      end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
