d9e3e99f17512ff9db75c488c5dd809fd017a725
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-02-23 13:57:10 -0300

    seeded DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.new
+  restaurant.name = Faker::Restaurant.name
+  restaurant.address = Faker::Address.street_address
+  restaurant.category = %w[chinese italian japanese french belgian].sample
+  restaurant.save
+end
